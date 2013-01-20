d19c7a62c9a9d2ef7ee8819378608f449a7b0a4a
magomimmo <mimmo.cosenza@gmail.com>
2013-01-21 00:03:42 +0100

    started adding friend

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,7 +1,19 @@
 (ns modern-cljs.core
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [resources not-found]]
-            [compojure.handler :refer [site]]))
+            [compojure.handler :refer [site]]
+            [cemerick.friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credential :as cred])))
+
+;; a dummy i-memory user db
+(def users {"root" {:username "mimmo.cosenza@gmail.com"
+                    :password "mimmo1"
+                    :roles #{::admin}}
+            "mimmo" {:username "giacomo.cosenza@sinapsi.com"
+                     :password "mimmo1"
+                     :roles @{::user}}})
+
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
