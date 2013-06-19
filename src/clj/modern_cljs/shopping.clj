693eba28d36e9603bf9fe6808e85943006a80366
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-19 16:20:13 +0200

    added shopping route

---
+++ src/clj/modern_cljs/shopping.clj
@@ -0,0 +1,40 @@
+(ns modern-cljs.shopping
+  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]))
+
+;; parse-int
+(defmulti parse-int type)
+(defmethod parse-int java.lang.Long [n] n)
+(defmethod parse-int java.lang.String [s] (Integer/parseInt s))
+
+;; parse-double
+(defmulti parse-double type)
+(defmethod parse-double java.lang.Double [d] d)
+(defmethod parse-double java.lang.Long [n] (double n))
+(defmethod parse-double java.lang.String [s] (Double/parseDouble s))
+
+(defn- calculate [quantity price tax discount]
+  (let [quantity (parse-int quantity)
+        price (parse-double price)
+        tax (parse-double tax)
+        discount (parse-double discount)]
+    (-> (* quantity price)
+        (* (+ 1 (/ tax 100)))
+        (- discount))))
+
+(deftemplate shopping "public/shopping.html"
+  [quantity price tax discount]
+     [:input#quantity] (set-attr :value quantity)
+     [:input#price] (set-attr :value price)
+     [:input#tax] (set-attr :value tax)
+     [:input#discount] (set-attr :value discount)
+     [:input#total] (set-attr :value
+                              (format "%,.2f" (calculate
+                                              quantity
+                                              price
+                                              tax
+                                              discount))))
+
+
+
+
+  
