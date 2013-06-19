f9e7ca517d2b928f33e6bd47e67f798fbf69d0a6
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-19 16:16:36 +0200

    added enlive 1.1.1

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
                  [domina "1.0.2-SNAPSHOT"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
                  [shoreleave/shoreleave-remote "0.3.0"]
-                 [com.cemerick/valip "0.3.2"]]
+                 [com.cemerick/valip "0.3.2"]
+                 [enlive "1.1.1"]]
 
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.5"]]
