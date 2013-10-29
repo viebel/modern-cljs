c3ad81119ab3f5b0d760a3b06d1fc31bef85f4b8
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-10-29 18:06:10 +0100

    upgrade to cljs r1978

--- project.clj
+++ project.clj
@@ -17,9 +17,9 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]
+                 [org.clojure/clojurescript "0.0-1978"]
                  [compojure "1.1.5"]
-                 [domina "1.0.2"]]
+                 [org.clojars.magomimmo/domina "1.0.3-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "0.3.4"]
             [lein-ring "0.8.7"]]
